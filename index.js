a37b0968ca2c5b06824b57bd43ad72beea7dccb7
vnllpe <52089595+vnllpe@users.noreply.github.com>
2021-04-24 00:48:48 +0300

    bbbigbeak

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const config = require('./config')
 const fs = require('fs')
 const {createCanvas, loadImage} = require('canvas')
 
-const VERSION = 'v1.3.2'
+const VERSION = 'v1.4.0'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomFromArray(arr) {
+  return arr[getRandomInt(arr.length)];
+}
 
 function makeFlag(arg, msg) {
   let canvas = createCanvas(400, 400)
@@ -52,7 +60,7 @@ client.on('message', msg => {
   let authorTag = msg.author.tag
   let authorUsername = msg.author.username
   let authorColor = msg.member.displayHexColor
-  let authorAvatar = msg.author.displayAvatarURL({format: 'png'})
+  let authorAvatar = msg.author.displayAvatarURL({dynamic: true})
 
   let getGuild = client.guilds.cache.get(msg.guild.id)
 
@@ -66,10 +74,41 @@ client.on('message', msg => {
         let embed = new Discord.MessageEmbed()
                                .setColor(`0x002d70`)
                                .setTitle('i have only two commands:')
-                               .setDescription('• l flag\n• l oldflag');
+                               .setDescription('• l flag\n• l oldflag')
 
         msg.channel.send(embed)
       }
+    }
+
+    if (text === 'bbbigbeak' || text === 'bbbigbonk' || text === 'bbbigbruh') {
+      let bigbeaks = ['https://cdn.discordapp.com/attachments/828678818960375810/835267239691157514/rfgm9eyp8o421-1.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835267239913062420/w-yUrvd9HiM.jpg',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835267240093548594/iLSp_sDvIPY.jpg',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835267240340750386/8Pm_qgTumfM.jpg',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835267240596078602/RMsdwOYnK0E.jpg',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835267241440182332/talkman-4e264b87d60a7.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835269605374033940/maxwell_s_emoticons_by_ianmata1998_dcjnqy1-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835269762913140776/E3199E43D5EB95189B3754F7A45A75F52031EFC7.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835269851509030922/maxwell_by_ianmata1998_dc4ljx3-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835269935142797333/maxwell_in_the_city_by_ianmata1998_dc6alxx-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270028936216576/8CF34F1275D51672CE8975D76625A4D807FD7EE6.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270162101567508/sleepy_maxwell_by_ianmata1998_dcjnrro-250t.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270254606286898/maxwell_founds_a_book_in_the_middle_of_the_street_by_ianmata1998_dc6al63-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270344603598898/maxwell_plays_the_pikopiko_by_ianmata1998_dcjns4i-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270368461062205/maxwell_in_home_by_ianmata1998_dc7f4m0-fullview.png',
+                      'https://cdn.discordapp.com/attachments/828678818960375810/835270396978004018/maxwell_feels_embarrassing_by_ianmata1998_dc6akch-fullview.png']
+
+      let choosenBigbeak = getRandomFromArray(bigbeaks)
+
+      let embed = new Discord.MessageEmbed()
+                             .setColor(`0x${authorColor}`)
+                             .setTitle('<:Ass:835233982554832966> Click me!')
+                             .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+                             .setImage(choosenBigbeak)
+                             .setTimestamp()
+	                           .setFooter(`Requested by ${authorUsername}`, authorAvatar)
+
+      msg.channel.send(embed)
     }
   }
 })
