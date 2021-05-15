32544d214b07ade36d62bb895e21f03ca1cd508d
vnllpe <52089595+vnllpe@users.noreply.github.com>
2021-05-16 00:53:49 +0300

    2 more bigbeaks

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config')
 const fs = require('fs')
 const {createCanvas, loadImage} = require('canvas')
 
-const VERSION = 'v1.9.0'
+const VERSION = 'v1.9.1'
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -59,6 +59,8 @@ function sendBbCommand(arg, authorColor, authorUsername, authorAvatar, msg) {
                     'https://cdn.discordapp.com/attachments/828678818960375810/835269935142797333/maxwell_in_the_city_by_ianmata1998_dc6alxx-fullview.png',
                     'https://cdn.discordapp.com/attachments/828678818960375810/835270028936216576/8CF34F1275D51672CE8975D76625A4D807FD7EE6.png',
                     'https://cdn.discordapp.com/attachments/828678818960375810/835270162101567508/sleepy_maxwell_by_ianmata1998_dcjnrro-250t.png',
+                    'https://cdn.discordapp.com/attachments/746104412803694712/843242512301817866/unknown-3.jpg',
+                    'https://cdn.discordapp.com/attachments/746104412803694712/843242512546267156/20210512_142917.jpg',
                     'https://cdn.discordapp.com/attachments/828678818960375810/835270254606286898/maxwell_founds_a_book_in_the_middle_of_the_street_by_ianmata1998_dc6al63-fullview.png',
                     'https://cdn.discordapp.com/attachments/828678818960375810/835270344603598898/maxwell_plays_the_pikopiko_by_ianmata1998_dcjns4i-fullview.png',
                     'https://cdn.discordapp.com/attachments/828678818960375810/835270368461062205/maxwell_in_home_by_ianmata1998_dc7f4m0-fullview.png',
